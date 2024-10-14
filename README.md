d75ef30bcc2b1058357af5c0a1bd0cf2887f4485
Dan Bailey <dbailey@danbailey.net>
2024-10-14 18:57:18 -0500

    first commit

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
 # evergreen-dental-theme
 WP theme for freelance client.
+
+# to-do list
+* top nav, including hamburger and search
+* footer
+* typography
+* hero slideshow
+* people grid
+* search results
---
+++ tailwind.config.js
@@ -0,0 +1,19 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [],
+  theme: {
+    colors: {
+      'forest': '#114e35',
+      'mint': '#cce2ce',
+      'latte': '#cca77c',
+      'steel': '#d9d9d9',
+      'blackish': '#040c1c'
+    },
+    fontFamily: {
+      'cabin': ['Cabin', 'Arial' 'sans-serif'],
+      'raleway': ['Raleway', 'Helvetica', 'Roboto', 'sans-serif'],
+    }
+  },
+  plugins: [],
+}
+
