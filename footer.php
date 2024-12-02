891c06770faf912226a4954fff6b22e048351814
Dan Bailey <dbailey@danbailey.net>
2024-12-01 22:57:33 -0600

    footer

--- footer.php
+++ footer.php
@@ -1,12 +1,71 @@
-</main>
-<?php get_sidebar(); ?>
-</div>
-<footer id="footer" role="contentinfo">
-<div id="copyright">
-&copy; <?php echo esc_html( date_i18n( __( 'Y', 'evergreen' ) ) ); ?> <?php echo esc_html( get_bloginfo( 'name' ) ); ?>
-</div>
-</footer>
-</div>
-<?php wp_footer(); ?>
+<footer>
+        <div class="container mx-auto text-center my-12">
+            <a href="https://pay.balancecollect.com/m/evergreendentalmn" class="pay-button mx-auto">Make a Payment</a>
+        </div>
+        <div class="container mx-auto flex flex-col md:flex-row justify-end self-end md:justify-between items-center space-y-6 md:space-y-0 p-4 bg-offwhite">
+            <img class="h-12 mt-0" src="<?php bloginfo('template_url'); ?>/assets/img/american-dental-association-logo.svg" alt="American Dental Association" />
+            <img class="h-12 mt-0" src="<?php bloginfo('template_url'); ?>/assets/img/top_dentist_hof.svg" alt="Twin Cities Top Dentist Hall of Fame" />
+            <img class="h-12 mt-0" src="<?php bloginfo('template_url'); ?>/assets/img/agf-logo.svg" alt="Academy of General Dentistry" />
+        </div>
+        <div class="bg-forest">
+            <div class="container mx-auto flex flex-col md:flex-row justify-center md:justify-between items-center space-y-6 p-4 bg-forest">
+                <div>
+                    <p class="text-offwhite text-left md:text-center">Evergreen Dental<br>
+                    7890 Mitchell Road<br>
+                    Eden Prairie, MN 55344<br><br>
+                    TEL: <a href="tel:9529377677">952-937-7677</a><br>
+                    FAX: 952-937-0232<br>
+                    <a href="mailto:smile@evergreendentalmn.com">smile@evergreendentalmn.com</a></p>
+                </div>
+                <div class="flex flex-col justify-center items-center space-y-4 p-4">
+                    <div class="flex flex-row justify-between items-center space-x-6 p-4">
+                        <a href="https://www.instagram.com/evergreendentalmn" target="_blank"><img src="<?php bloginfo('template_url'); ?>/assets/img/instagram.svg" alt="Instagram" class="h-8 w-8"></a>
+                        <a href="https://www.facebook.com/profile.php?id=100063811981573" target="_blank"></a><img src="<?php bloginfo('template_url'); ?>/assets/img/facebook.svg" alt="Facebook" class="h-8 w-8"></a>
+                    </div>
+                    <div class="text-offwhite">
+                        <a class="footer-link-button" href="https://www.patientconnect365.com/Dentists/Minnesota/EdenPrairie/55344/Evergreen_Dental#" target="_blank">Patient Reviews</a>
+                    </div>
+                </div>
+            </div>
+        </div>
+        
+    </footer>
+
+    <script type="application/ld+json">
+        {
+          "@context": "https://schema.org",
+          "@type": "LocalBusiness",
+          "name": "Evergreen Dental",
+          "image": "ImageURL",
+          "@id": "",
+          "url": "http://url",
+          "telephone": "phone",
+          "address": {
+            "@type": "PostalAddress",
+            "streetAddress": "Street",
+            "addressLocality": "City",
+            "addressRegion": "MN",
+            "postalCode": "ZIP",
+            "addressCountry": "US"
+          },
+          "geo": {
+            "@type": "GeoCoordinates",
+            "latitude": lat,
+            "longitude": long
+          },
+          "openingHoursSpecification": {
+            "@type": "OpeningHoursSpecification",
+            "dayOfWeek": [
+              "Monday",
+              "Tuesday",
+              "Wednesday",
+              "Thursday",
+              "Friday"
+            ],
+            "opens": "09:00",
+            "closes": "17:00"
+          } 
+        }
+    </script>
 </body>
 </html>
