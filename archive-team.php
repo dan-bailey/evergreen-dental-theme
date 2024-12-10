77c08fdc61f14894bc83e2c38570f27f26e3bf5a
Dan Bailey <dbailey@danbailey.net>
2024-12-09 22:39:55 -0600

    team stuff done

--- archive-team.php
+++ archive-team.php
@@ -4,20 +4,30 @@
  <section>
         <div class="container mx-auto my-6">
         
-            <div class="grid gap-8 mb-6 lg:mb-16 grid-cols-1 lg:grid-cols-2">
-            <?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>    
+            <div class="grid gap-8 mb-6 lg:mb-16 grid-cols-1 sm:grid-cols-2">
+
+            <?php
+            $args = array(
+                'post_type' => 'team',
+                'posts_per_page' => -1,
+                'orderby' => 'menu_order',
+                'order' => 'ASC'
+            );
+            $the_query = new WP_Query ($args); 
+            if ( $the_query->have_posts() ) : 
+                while ( $the_query->have_posts() ) : 
+                    $the_query->the_post(); 
+            ?>    
                 <!-- team card (repeats) -->
                 <div class="items-center bg-white rounded-lg shadow sm:flex">
                     <a href="<?php the_permalink(); ?>">
-                    <img class="block sm:hidden rounded-lg sm:rounded-none sm:rounded-l-lg" src="<?php the_post_thumbnail_url("small"); ?>" alt="<?php the_title(); ?>" />
-                        <img class="hidden md:block xl:hidden rounded-lg sm:rounded-none sm:rounded-l-lg" src="<?php the_post_thumbnail_url("small"); ?>" alt="<?php the_title(); ?>" />
-                        <img class="hidden xl:block rounded-lg sm:rounded-none sm:rounded-l-lg" src="<?php the_post_thumbnail_url("medium"); ?>" alt="<?php the_title(); ?>" />
+                        <img class="w-full rounded-lg sm:rounded-none sm:rounded-l-lg" src="<?php the_post_thumbnail_url("medium"); ?>" alt="<?php the_title(); ?>" />
                     </a>
-                    <div class="p-5">
+                    <div class="w-full sm:w-1/2 p-5">
                         <h3 class="text-2xl tracking-tight text-blackish">
                             <a href="<?php get_the_permalink(); ?>"><?php the_title(); ?></a>
                         </h3>
-                        <span class="text-gray-500"><?php the_field('job'); ?></span>
+                        <p class="text-gray-500"><?php the_field('job'); ?></p>
                         <p class="mt-1 mb-4"><?php the_field('card_blurb'); ?></p>
                         <p class="inline-block p-2 bg-forest text-offwhite shadow rounded-lg"><a href="<?php the_permalink(); ?>">Learn More</a></p>
                     </div>
--- single-team.php
+++ single-team.php
@@ -1,1 +1,11 @@
 <!-- single-team.php -->
+<?php get_header(); ?>
+<?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
+
+<div id="team-member" class="container mx-auto my-6 px-2.5">
+    <h2 class="text-4xl text-blackish"><?php the_title(); ?></h2>
+    <?php the_content(); ?>
+</div>
+<?php if ( comments_open() && !post_password_required() ) { comments_template( '', true ); } ?>
+<?php endwhile; endif; ?>
+<?php get_footer(); ?>
